7af9c29c53c65086fcb734389a589caa8315d42c
coepedepano <84387478+coepedepano@users.noreply.github.com>
2021-05-19 13:33:29 -0300

    config v1.0

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('cryelo app is running!');
+    expect(await page.getTitleText()).toEqual('vipclass app is running!');
   });
 
   afterEach(async () => {
